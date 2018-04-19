6cfada15c64b12f7d68cdefe8faaa12458664fbc
Yash Kumar Prajapati <yashkumar.prajapati@gmail.com>
2018-04-19 22:44:13 +0530

    Task: Reportee Goal Design

--- AngularApp/src/app/account/reportee-goal/reportee-goal.component.ts
+++ AngularApp/src/app/account/reportee-goal/reportee-goal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-reportee-goal',
@@ -11,17 +13,31 @@ export class ReporteeGoalComponent implements OnInit {
   reporteeId: number;
   reporteeName: string;
   reporteeEmail: string;
+  goalModel: any;
+
+  model: NgbDateStruct;
+  date: { year: number, month: number };
+  btnUpdate: boolean = false;
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+
+    this.goalModel = new FormGroup({
+      goal: new FormControl('', Validators.required),
+      weight: new FormControl('', Validators.required),
+      desc: new FormControl('', Validators.required),
+      stdate: new FormControl('', Validators.required),
+      enddate: new FormControl('', Validators.required),
+      reveiwer: new FormControl('', Validators.required)
+    });
 
     let data = atob(this.activatedRoute.snapshot.queryParams['data']).split(':');
 
     this.reporteeId = Number(data[0]);
     this.reporteeName = data[1];
     this.reporteeEmail = data[2];
-    alert(this.reporteeId);
+    // alert(this.reporteeName);
   }
 
 }
